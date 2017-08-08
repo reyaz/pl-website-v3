1e1cb8d52efb29fb402507fa27dea6b672832e4d
Reyaz Rohani <reyaz@pulselocker.com>
2017-08-08 11:09:50 -0700

    [PABU-795] Add compression middleware.

--- src/server/main.js
+++ src/server/main.js
@@ -1,3 +1,4 @@
+import compression from 'compression'
 import express from 'express'
 import path from 'path'
 
@@ -5,6 +6,8 @@ import strings from '../imports/strings.json'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(compression())
 
 app.use('/assets', express.static(path.join(__dirname, '../assets')))
 app.use('/assets', express.static(path.join(__dirname, '../../build/client')))
